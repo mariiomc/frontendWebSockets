a7b416e8fd25c45967081a07f8c5ea3617507e7f
borjafernandez35 <borjaf29@gmail.com>
2024-05-21 18:56:29 +0200

    it works

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -8,37 +8,33 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:web_socket_flutter/controller/chat_controller.dart';
 import 'package:web_socket_flutter/model/message.dart';
 
-
 class ChatScreen extends StatefulWidget {
-  const ChatScreen( {Key? key}) : super(key: key);
-
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
-    _ChatScreenState createState() => _ChatScreenState();
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen>{
+class _ChatScreenState extends State<ChatScreen> {
   Color purple = Color.fromARGB(255, 108, 92, 231);
   Color black = Color.fromARGB(255, 5, 5, 5);
   TextEditingController msgInputController = TextEditingController();
-    TextEditingController textController = TextEditingController();
+  late IO.Socket socket;
+  ChatController chatController = ChatController();
 
-late IO.Socket socket;
-ChatController chatController = ChatController();
-
-@override
-  // void initState() {
-  //   socket = IO.io(
-  //     'http://localhost:4000',
-  //      IO.OptionBuilder()
-  //         .setTransports(['websocket'])
-  //         .disableAutoConnect()
-  //         .build());
-  //   socket.connect();
-  //   setUpSocketListener();
-  //   super.initState();
-  //   print("Socket connected: ${socket.connected} --- Socket id: ${socket.id}");
-  // }
+  @override
+  void initState() {
+    socket = IO.io(
+        'http://localhost:4000',
+        IO.OptionBuilder()
+            .setTransports(['websocket'])
+            .disableAutoConnect()
+            .build());
+    socket.connect();
+    setUpSocketListener();
+    super.initState();
+    print("Socket connected: ${socket.connected} --- Socket id: ${socket.id}");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,26 +46,26 @@ ChatController chatController = ChatController();
             Expanded(
               child: Obx(
                 () => Container(
-                padding: EdgeInsets.all(10),
-              child: Text("Connected User ${chatController.connectedUser}",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15.0,
-              )
-              ),
+                  padding: EdgeInsets.all(10),
+                  child: Text("Connected User ${chatController.connectedUser}",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      )),
                 ),
-            ),
+              ),
             ),
             Expanded(
               flex: 9,
               child: Obx(
                 () => ListView.builder(
                   itemCount: chatController.chatMessages.length,
-                  itemBuilder: (context, index){
+                  itemBuilder: (context, index) {
                     var currentItem = chatController.chatMessages[index];
-                  return MessageItem(
-                    sentByMe: currentItem.sentByMe == socket.id,
-                    message: currentItem.message,
+                    return MessageItem(
+                      sentByMe: currentItem.sentByMe == socket.id,
+                      message: currentItem.message,
+                    
                     );
                   },
                 ),
@@ -80,134 +76,88 @@ ChatController chatController = ChatController();
                 padding: EdgeInsets.all(10),
                 color: Colors.red,
                 child: TextField(
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                  cursorColor: purple,
-                  controller: msgInputController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:  BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:  BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    suffixIcon: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color:purple,
-                      ),
-                      //color: purple,
-                      child: IconButton(
-                        onPressed: () {
-                          sendMessage(msgInputController.text);
-                          msgInputController.text="";
-                        },
-                        icon: Icon(Icons.send, color: Colors.white,),
-                      )
-                    )
-                  )
-                ),
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: purple,
+                    controller: msgInputController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        suffixIcon: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: purple,
+                            ),
+                            //color: purple,
+                            child: IconButton(
+                              onPressed: () {
+                                sendMessage(msgInputController.text);
+                                msgInputController.text = "";
+                              },
+                              icon: Icon(
+                                Icons.send,
+                                color: Colors.white,
+                              ),
+                            )))),
               ),
             ),
-            Expanded(
-            child: Row(
-              children: [
-                // Campo de entrada de texto
-                Expanded(
-                  child: TextField(
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    controller: textController, // Un controlador para capturar el texto introducido
-                    decoration: InputDecoration(
-                      hintText: 'Introduce un texto',
-                      hintStyle: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10), // Separación entre el campo de entrada de texto y el botón
-                // Botón
-                Container(
-                  child: IconButton(
-                        onPressed: () {
-                          createConnection(textController.text);
-                          textController.text="";
-                        },
-                        icon: Icon(Icons.send, color: Colors.white,),
-                      )
-                ),
-              ],
-            ),
-          )
-
           ],
         ),
       ),
     );
   }
-  
+
   void sendMessage(String text) {
     print(socket);
     print(socket.id);
     print(socket.connected);
-  var messageJson = {
-    "message": text,
-    "sentByMe": socket.id ?? "" // Asigna una cadena vacía si socket.id es nulo
-  };
-  print('Mensaje: $messageJson');
-  socket.emit('message', messageJson);
-  chatController.chatMessages.add(Message.fromJson(messageJson));
-}
-
-void createConnection(String room) {
-    socket = IO.io(
-      'http://localhost:4000/${room}',
-       IO.OptionBuilder()
-          .setTransports(['websocket'])
-          .disableAutoConnect()
-          .build());
-    socket.connect();
-    setUpSocketListener();
-    super.initState();
-    print("Socket connected: ${socket.connected} --- Socket id: ${socket.id}");
+    print(socket.json);
+    var messageJson = {
+      "message": text,
+      "sentByMe":
+          socket.id ?? "", // Asigna una cadena vacía si socket.id es nulo
+    
+    };
+    print('Mensaje: $messageJson');
+    socket.emit('message', messageJson);
+    chatController.chatMessages.add(Message.fromJson(messageJson));
   }
 
   void setUpSocketListener() {
-    socket.on('message-receive', (data){
+    socket.on('message-receive', (data) {
       print(data);
+      
+
       chatController.chatMessages.add(Message.fromJson(data));
     });
-    socket.on('connected-user', (data){
+    socket.on('connected-user', (data) {
       print(data);
       chatController.connectedUser.value = data;
-  });
+    });
+    socket.on('room', (data) {
+      print(data);
+      chatController.connectedUser.value = data;
+    }); 
   }
 }
 
 class MessageItem extends StatelessWidget {
-  const MessageItem({Key? key, required this.sentByMe, required this.message})
+  const MessageItem(
+      {Key? key,
+      required this.sentByMe,
+      required this.message,
+      })
       : super(key: key);
 
   final bool sentByMe;
   final String message;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -236,13 +186,13 @@ class MessageItem extends StatelessWidget {
               ),
             ),
             SizedBox(width: 5),
-            Text(
-              "1:10 AM",
+            /* Text(
+              date as String,
               style: TextStyle(
                 color: (sentByMe ? white : purple).withOpacity(0.7),
                 fontSize: 10,
               ),
-            )
+            ) */
           ],
         ),
       ),
